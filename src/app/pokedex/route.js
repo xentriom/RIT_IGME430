bf7247ac28f60e2c52a2b10086d8243650d79087
xentriom <c04jason@gmail.com>
2026-02-17 18:12:19 -0500

    dev script and pokedex route

---
+++ src/app/pokedex/route.js
@@ -0,0 +1,14 @@
+const { respond } = require('../../utils/respond')
+const pokedex = require('../../../client/pokedex.json')
+
+const GET = (req, res) => {
+  respond(req, res, 200, 'application/json', pokedex)
+}
+const HEAD = (req, res) => {
+  respond(req, res, 200, 'application/json', pokedex)
+}
+
+module.exports = {
+  GET,
+  HEAD,
+}
