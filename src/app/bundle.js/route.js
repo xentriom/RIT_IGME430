3000843d602ce1f73dcc92c84d21b65623e7aae5
xentriom <c04jason@gmail.com>
2026-02-17 15:19:40 -0500

    adjusted the base

---
+++ src/app/bundle.js/route.js
@@ -0,0 +1,12 @@
+const { readFileSync } = require('fs');
+const { respond } = require('../../utils/index.js');
+
+const script = readFileSync(`${__dirname}/../../../hosted/bundle.js`, 'utf-8');
+
+const GET = (req, res) => {
+  respond(req, res, 200, 'application/javascript', script);
+};
+
+module.exports = {
+  GET,
+};
--- src/app/route.js
+++ src/app/route.js
@@ -1,9 +1,10 @@
+const { readFileSync } = require('fs');
 const { respond } = require('../utils/index.js');
 
+const index = readFileSync(`${__dirname}/../../hosted/client.html`, 'utf-8');
+
 const GET = (req, res) => {
-  respond(req, res, 200, 'application/json', {
-    message: 'Hello',
-  });
+  respond(req, res, 200, 'text/html', index);
 };
 
 module.exports = {
