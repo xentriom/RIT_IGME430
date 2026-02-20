0c313a12d18b927c5720470c4eb3a6e75ce283c5
xentriom <c04jason@gmail.com>
2026-02-20 16:47:32 -0500

    fix wrong path

--- client/main.tsx
+++ client/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
-import { initTheme } from "../test/docs/utils/theme";
+import { initTheme } from "./utils/theme";
 import "./globals.css";
 
 initTheme();
