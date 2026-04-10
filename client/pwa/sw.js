1bc46172c1b9340e7d8063feffd21f67124976dd
xentriom <c04jason@gmail.com>
2026-04-10 16:04:05 -0400

    service workers?

--- client/pwa/sw.js
+++ client/pwa/sw.js
@@ -1,7 +1,61 @@
+https://developer.chrome.com/docs/workbox/service-worker-lifecycle#the_lifecycle_of_a_new_service_worker
+const cacheKey = "Chitter_v1";
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+    caches.open(cacheKey).then((cache) => {
+      // Add all static assets to the cache
+      return cache.addAll([
+        "/assets/icons/android-icon-192x192.png",
+        "/assets/icons/android-icon-512x512.png",
+        "/assets/icons/apple-icon-180x180.png",
+        "/assets/icons/favicon-16x16.png",
+        "/assets/icons/favicon-32x32.png",
+        "/assets/icons/favicon.ico",
+        "/assets/manifest.json",
+      ]);
+    }),
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  const cacheAllowList = [cacheKey];
+
+  // Get all the currently active `Cache` instances.
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      // Delete all caches that aren't in the allow list:
+      return Promise.all(
+        keys.map((key) => {
+          if (!cacheAllowList.includes(key)) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+});
+
+/**
+ * https://developer.chrome.com/docs/workbox/caching-strategies-overview#stale-while-revalidate
+ * Does not cache non-GET reqs or non-origin requests
+ */
+self.addEventListener("fetch", (event) => {
+  const { request } = event;
+  if (request.method !== "GET") return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.open(cacheKey).then(async (cache) => {
+      return cache.match(request).then((cachedResponse) => {
+        const fetchResponse = fetch(request).then((networkResponse) => {
+          cache.put(request, networkResponse.clone());
+          return networkResponse;
+        });
+        return cachedResponse || fetchResponse;
+      });
+    }),
+  );
 });
--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,43 @@
-self.addEventListener("install",i=>{i.waitUntil(self.skipWaiting())}),self.addEventListener("activate",i=>{i.waitUntil(self.clients.claim())});
+const cacheKey = "Chitter_v1";
+(self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(cacheKey)
+      .then((e) =>
+        e.addAll([
+          "/assets/icons/android-icon-192x192.png",
+          "/assets/icons/android-icon-512x512.png",
+          "/assets/icons/apple-icon-180x180.png",
+          "/assets/icons/favicon-16x16.png",
+          "/assets/icons/favicon-32x32.png",
+          "/assets/icons/favicon.ico",
+          "/assets/manifest.json",
+        ]),
+      ),
+  );
+}),
+  self.addEventListener("activate", (e) => {
+    const n = [cacheKey];
+    e.waitUntil(
+      caches.keys().then((e) =>
+        Promise.all(
+          e.map((e) => {
+            if (!n.includes(e)) return caches.delete(e);
+          }),
+        ),
+      ),
+    );
+  }),
+  self.addEventListener("fetch", (e) => {
+    const { request: n } = e;
+    "GET" === n.method &&
+      new URL(n.url).origin === self.location.origin &&
+      e.respondWith(
+        caches.open(cacheKey).then(async (e) =>
+          e.match(n).then((s) => {
+            const t = fetch(n).then((s) => (e.put(n, s.clone()), s));
+            return s || t;
+          }),
+        ),
+      );
+  }));
